c120f89dee50fec837f80047ec815549df240d1c
Roman <romaholovai@gmail.com>
2025-04-09 16:46:11 +0200

    init

---
+++ O2Test/O2Test/O2TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  O2TestApp.swift
+//  O2Test
+//
+//  Created by Roman on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct O2TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
